f3090fee9e921022e1d156ab27a38008c47b2d39
Carl <callmer95@gmail.com>
2017-05-13 08:40:31 +0200

    setting everything up

---
+++ README.md
@@ -0,0 +1,1 @@
+The freecodecamp d3 heatmap challange
---
+++ src/js/client.js
@@ -0,0 +1,16 @@
+
+import "../styles/main.css"
+
+import * as d3 from "d3"
+import axios from "axios"
+
+const URL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+
+document.getElementById("test").innerHTML = "Paragraph changed!";
+
+// axios.get(URL)
+
+// 	})
+// 	.catch(function (error) {
+//     console.error(error);
+//   });
