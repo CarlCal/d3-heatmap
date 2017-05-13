b8a25b679243eafaf5c2f064c8de4f5d11139a33
Carl <callmer95@gmail.com>
2017-05-13 16:43:01 +0200

    the axis are working

--- src/js/client.js
+++ src/js/client.js
@@ -5,8 +5,8 @@ import * as d3 from "d3"
 import axios from "axios"
 
 const URL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",
-      colors = ["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"],
-      months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]  
+      months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],  
+      colors = ["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"]
 
 axios.get(URL)
   .then(function (response) {
@@ -20,9 +20,11 @@ axios.get(URL)
 
     var chart = d3.select(".chart")
 
+    var minYear = d3.min(data, function(d) { return d.year }),
+        maxYear = d3.max(data, function(d) { return d.year })
 
     var x = d3.scaleLinear().range([0, width])
-        .domain([d3.min(data, function(d) { return d.year }), d3.max(data, function(d) { return d.year })])
+        .domain([minYear, maxYear])
 
     var y = d3.scaleLinear().range([0, height])
         .domain([0, d3.max(data, function(d) { return d.month })])
@@ -30,34 +32,29 @@ axios.get(URL)
     var g = chart.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-
     g.append("g")
-      .attr("class", "axis--x")
+      .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + (height + 0) + ")")
-      .call(d3.axisBottom(x))
+      .call(d3.axisBottom(x).ticks(26).tickFormat(d3.format("")))
     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -70)
-      .attr("x", (-height/2))
-      .attr("dx", "1.80em")
+      .attr("x", (width/2))
+      .attr("y", 70)
       .attr("text-anchor", "center")
       .attr("fill", "black")
       .style("font-size", "18px")
-      .style("font-weight", "bold")
-      .text("Months")
+      .text("Years")
 
     g.append("g")
-      .attr("class", "axis--y")
-      .call(d3.axisLeft(y))
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(12).tickFormat(function(d, i) { return months[i] }))
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", -70)
       .attr("x", (-height/2))
+      .attr("y", -70)
       .attr("dx", "1.80em")
       .attr("text-anchor", "center")
       .attr("fill", "black")
       .style("font-size", "18px")
-      .style("font-weight", "bold")
       .text("Months")
 
 
