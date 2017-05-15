bb0901baf1e1bd652583c0a71f9dccaebeb84b3e
Carl <callmer95@gmail.com>
2017-05-15 15:20:06 +0200

    the main part of the map works

--- src/js/client.js
+++ src/js/client.js
@@ -16,7 +16,9 @@ axios.get(URL)
 
     const margin = { top: 10, right: 10, bottom: 90, left: 90 },
           width = 1200 - margin.left - margin.right,
-          height = 550 - margin.top - margin.bottom
+          height = 550 - margin.top - margin.bottom,
+          legendWidth = 40,
+          legendHeight = 20
 
     var chart = d3.select(".chart")
 
@@ -27,32 +29,34 @@ axios.get(URL)
         .domain([minYear, maxYear])
 
     var y = d3.scaleBand().range([0, height])
-        .domain(data.map(function(d) { return (d.month-1) }))
+        .domain(data.map(function(d) { return (d.month - 1) }))
 
     var g = chart.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
     /*Appending a x axis*/
-    g.append("g")
+    var xAxis = g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + (height + 1) + ")")
       .call(d3.axisBottom(x).ticks(26).tickFormat(d3.format("")))
     .append("text")
-      .attr("x", (width/2))
-      .attr("y", 70)
+      .attr("x", (width / 2))
+      .attr("y", 45)
       .attr("text-anchor", "center")
       .attr("fill", "black")
       .style("font-size", "18px")
       .text("Years")
 
+    xAxis.exit().remove()
+
     /*Appending a y axis*/
-    g.append("g")
+    var yAxis = g.append("g")
       .attr("class", "axis axis--y")
       .attr("transform", "translate(-1, 0)")
       .call(d3.axisLeft(y).ticks(12).tickFormat(function(d, i) { return months[i] }))
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("x", (-height/2))
+      .attr("x", (-height / 2))
       .attr("y", -70)
       .attr("dx", "1.80em")
       .attr("text-anchor", "center")
@@ -60,25 +64,59 @@ axios.get(URL)
       .style("font-size", "18px")
       .text("Months")
 
-    var cards = g.selectAll(".temp")
-        .data(data)
+    yAxis.exit().remove()
 
-    cards.enter().append("rect")
+    var totalTemp = data.map(function(d) { return baseTemp + d.variance })
+
+    var formatLabel = d3.format("0.2f")
+
+    var colorScale = d3.scaleQuantile()
+      .domain([d3.min(totalTemp), d3.max(totalTemp)])
+      .range(colors)
+    
+    var cards = g.append("g")
+        .attr("class", "cards")
+      .selectAll(".temp")
+        .data(data)
+        .enter()
+      .append("rect")
         .attr("x", function(d) { return x(d.year) })
-        .attr("y", function(d) { return y(d.month-1) })
+        .attr("y", function(d) { return y(d.month - 1) })
         .attr("class", "temp")
         .attr("width", 5)
-        .attr("height", y.bandwidth()+1)
-        .style("fill", colors[0])
+        .attr("height", y.bandwidth() + 1)
+        .style("fill", function(d) { return colorScale(baseTemp+d.variance)})
 
-    // var colorScale = d3.scaleQuantile()
-    //     .domain([baseTemp, colors.length - 1, d3.max(data, function(d) { return d.variance })])
-    //     .range(colors)
+    cards.exit().remove()
 
-    // cards.transition().duration(1000)
-    //   .style("fill", function(d) {return colorScale(d.variance) })
+    var legend = g.append("g")
+        .attr("class", "legendSpectrum")
+      .selectAll(".legend")
+        .data([0].concat(colorScale.quantiles()), function(d) { return d })
+        .enter()
+      .append("g")
+        .attr("class", "legend")
 
-    // cards.exit().remove()
+    legend.append("rect")
+        .attr("x", function(d, i) { return (width-(legendWidth*colors.length))+(i*legendWidth) })
+        .attr("y", height + 50)
+        .attr("width", legendWidth)
+        .attr("height", legendHeight)
+        .style("fill", function(d, i) { return colors[i] })
+    
+    legend.append("text")
+        .attr("class", "legendLabel")
+        .attr("x", function(d, i) { return (width-(legendWidth*colors.length))+(i*legendWidth) + 5 })
+        .attr("y", height + legendHeight + 65)
+        .attr("fill", "black")
+        .attr("text-anchor", "center")
+        .style("font-size", "14px")
+        .text(function(d) { return formatLabel(d) })
+
+    legend.exit().remove()
+
+
+    /*TOOL TIP*/
 
   })
   .catch(function (error) {
